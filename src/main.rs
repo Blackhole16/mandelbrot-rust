3da001a274eeecf5cfb229b936e1a38fabd42e9d
Blackhole16 <Blackhole16@web.de>
2015-08-10 22:43:37 +0200

    add improved rgb

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() {
     const ISTART: f64 = -1.5;
     const IEND: f64 = 1.5;
 
-    const WIDTH: i32 = 1920;
-    const HEIGHT: i32 = 1080;
+    const WIDTH: i32 = 1600;
+    const HEIGHT: i32 = 1200;
 
     const RD: f64 = (REND - RSTART) / WIDTH as f64;
     const ID: f64 = (IEND - ISTART) / HEIGHT as f64;
@@ -38,14 +38,16 @@ fn main() {
     let imgbuf = ImageBuffer::from_fn(WIDTH as u32, HEIGHT as u32, |x,y| {
         let i = arr[y as usize][x as usize] as f64;
         let id = i / ITERATIONS as f64;
-        let ir = id.powf(0.01);
+        let ir = id.powf(0.1);
         let ic = ir * 255f64;
-        image::Luma([ic as u8]) 
+        let r = (x as f64) / (WIDTH as f64) * 255f64;
+        let g = (y as f64) / (HEIGHT as f64) * 255f64;
+        image::Rgb([r as u8, g as u8, ic as u8]) 
     });
 
     // Save the image as “fractal.png”
     let mut file = File::create("fractal.png").unwrap();
 
     // We must indicate the image’s color type and what format to save as
-    image::ImageLuma8(imgbuf).save(&mut file, image::PNG);
+    image::ImageRgb8(imgbuf).save(&mut file, image::PNG);
 }
